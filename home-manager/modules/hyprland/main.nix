27e37fc56f56f59b0d6da69cb699ccbd15884714
wqqzxts <wqqzxts@gmail.com>
2025-12-24 00:29:39 +0800

    increase transparency hypr

--- home-manager/modules/hyprland/main.nix
+++ home-manager/modules/hyprland/main.nix
@@ -43,8 +43,8 @@
       decoration = {
         rounding = 20;
 
-        active_opacity = 0.9;
-        inactive_opacity = 0.9;
+        active_opacity = 0.75;
+        inactive_opacity = 0.75;
 
         shadow = {
           enabled = false;
